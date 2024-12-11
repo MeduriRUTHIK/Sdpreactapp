2ac0a8bf8d653dd18dec43cb436adafd330e4b2a
Ruthik_6 <73951152+MeduriRUTHIK@users.noreply.github.com>
2024-12-11 17:54:51 +0530

    Update ViewReciepients.jsx

--- src/assets/components/ViewReciepients.jsx
+++ src/assets/components/ViewReciepients.jsx
@@ -12,7 +12,7 @@ const ViewReciepients = () => {
   useEffect(() => {
     const fetchDonationRequests = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/allreciepients");
+        const response = await axios.get("sdpdeployment.up.railway.app/allreciepients");
         setDonationRequests(response.data);
       } catch (error) {
         console.error("Error fetching donation requests:", error);
